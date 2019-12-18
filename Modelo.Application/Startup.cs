7d2c9b58036a088389b20acb73a7d96c58fc2847
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2019-12-18 17:52:20 -0300

    :pencil: Refina estrutura

--- Modelo.Application/Startup.cs
+++ Modelo.Application/Startup.cs
@@ -13,18 +13,12 @@ namespace Modelo.Application
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
+        public void ConfigureServices(IServiceCollection services) => services.AddMvc();
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
